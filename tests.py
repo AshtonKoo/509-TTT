3405edf4acdac22be8ffd20a95663595ae33aaac
AshtonKoo <148400739+AshtonKoo@users.noreply.github.com>
2023-11-07 18:27:57 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,19 +1,16 @@
-import unittest
-import logic
+from logic import get_winner
 
+# X win
+test_board = [[None,"O","X"],["O",None,"X"],[None,None,"X"]]
+winner = get_winner(test_board)
+print(winner)
 
-class TestLogic(unittest.TestCase):
+# O win
+test_board = [["O","O","X"],["O",None,"X"],["O",None,"X"]]
+winner = get_winner(test_board)
+print(winner)
 
-    def test_get_winner(self):
-        board = [
-            ['X', None, 'O'],
-            [None, 'X', None],
-            [None, 'O', 'X'],
-        ]
-        self.assertEqual(logic.get_winner(board), 'X')
-
-    # TODO: Test all functions from logic.py!
-
-
-if __name__ == '__main__':
-    unittest.main()
+# Nobody win
+test_board = [[None,"O","X"],["O",None,"X"],["O",None,"O"]]
+winner = get_winner(test_board)
+print(winner)
